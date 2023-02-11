46bf73d7ca902c589c16ddcfb84ebdfd7be07ae7
moch akbar maulana <moc.akbar119@gmail.com>
2023-02-11 15:43:17 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/simple_movies_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simple_movies_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrlW500 = 'https://image.tmdb.org/t/p/w500';
+  static const String imageUrlOriginal = 'https://image.tmdb.org/t/p/original';
+  static const String apiKey = '4dda69557f9d1b4d4930dd9ee950047d';
+}
